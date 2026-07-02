b16bc069626a641ac3609c746033534ab2359aeb
cwyz-dev <chris@wwwoods.xyz>
2026-07-01 21:07:22 -0600

    Update?

--- kubernetes/archbase/build.sh
+++ kubernetes/archbase/build.sh
@@ -1,6 +1,5 @@
 PROJECT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 PROJECT_NAME="$(basename $PROJECT_DIR)"
-SUPER_NAME="$1"
 podman build \
-	-f "$PROJECT_DIR/Containerfile" \
-	-t "custom-$SUPER_NAME-$PROJECT_NAME:latest"
+	-f "${PROJECT_DIR}/Containerfile" \
+	-t "custom-${PROJECT_NAME}:latest"
